a79cfe24d2f18aaafb91a6a4e44e7df1715cbc91
ray_delpino <raydelpino29@gmail.com>
2017-10-24 14:49:26 -0400

    add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,29 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user,
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors,
+  };
+};
+
+export const login = (user) => {
+  return (dispatch) => {
+    return SessionApiUtil.login(user).then((user) => {
+      dispatch(receiveCurrentUser(user));
+    },
+    (errors) => {
+      dispatch(receiveErrors(errors));
+    });
+  };
+};
