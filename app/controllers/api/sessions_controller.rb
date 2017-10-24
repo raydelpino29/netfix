ed413de69374690f498d59faad18cf7b93a3d1b2
ray_delpino <raydelpino29@gmail.com>
2017-10-24 13:53:31 -0400

    migrate users table

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login!(@user)
+      render :show
+    else
+      render json: ["Invalid Username/Password"], status: 401
+  end
+
+  def destroy
+    @current_user = current_user
+    if @current_user
+      logout!
+    else
+      render json: ["No User is Signed In"], status: 404
+    end
+  end
+
+end
--- db/migrate/20171024154355_create_users.rb
+++ db/migrate/20171024154355_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username, null: false
       t.string :password_digest, null: false
+      t.string :session_token, null: false
     end
   end
 end
