003626b05b7a4d9e5a7a07a056a8799cac6ba8bf
Sally <wsaehee21@outlook.com>
2017-10-24 15:50:48 -0400

    fix user model for before_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 validates :username, :session_token, presence: true, uniqueness: true
 validates :password_digest, presence: true
-before_action :ensure_session_token
+before_validation :ensure_session_token
 
 attr_reader :password
 
