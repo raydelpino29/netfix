901b1b322ff1bb4af0777a52c921e64617086dcc
ray_delpino <raydelpino29@gmail.com>
2017-10-25 17:50:17 -0400

    add demo user into seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025134725) do
+ActiveRecord::Schema.define(version: 20171025214011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171025134725) do
   create_table "users", force: :cascade do |t|
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
