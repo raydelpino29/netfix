5d9a2af70656d971d1f528d556bed636c6ff8188
ray_delpino <raydelpino29@gmail.com>
2017-10-27 18:45:28 -0400

    interpolate state to assign class to video icons

--- frontend/components/videos/video_item.jsx
+++ frontend/components/videos/video_item.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 class VideoItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { value: { like: false, dislike: false, myList: false }, class: { like: "icon fa fa-thumbs-o-up", dislike: "icon fa fa-thumbs-o-down", myList: "icon fa fa-plus-circle" } };
+  }
+
+  handleClick(field) {
+    return (e) => {
+      this.setState({ [field]: !this.state.field });
+    };
   }
 
   render () {
     return (
       <li className='video-item-container'>
         <img className='video-item' src={this.props.video.thumbnail_url} />
-        <i className="icon fa fa-thumbs-o-up"></i>
-        <i className="icon fa fa-thumbs-o-down"></i>
+        <i onClick={this.handleClick('like')} className={this.state.class.like}></i>
+        <i onClick={this.handleClick('dislike')} className={this.state.class.dislike}></i>
+        <i onClick={this.handleClick('myList')} className={this.state.class.myList}></i>
       </li>
     )
   }
