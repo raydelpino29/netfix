1b2ef9404a76d86e763280193c3e47997f181741
ray_delpino <raydelpino29@gmail.com>
2017-10-27 12:03:22 -0400

    add vh units to background height

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -4,10 +4,18 @@ const Header = () => {
   return (
     <nav>
       <ul className="header">
-        <li className='logo' >Netfix</li>
-        <li className='browse'>Browse</li>
-        <li className='search'>Search</li>
-        <li className='profile'>Profile</li>
+        <li>
+          <ul className="header-left">
+            <li className="logo">Netfix</li>
+            <li className='browse'>Browse</li>
+          </ul>
+        </li>
+        <li>
+          <ul className='header-right'>
+            <li className='search'>Search</li>
+            <li className='profile'>Profile</li>
+          </ul>
+        </li>
       </ul>
     </nav>
   );
