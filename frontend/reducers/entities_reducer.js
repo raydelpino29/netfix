75ad11ba8dbaa111f791176b92fa03306863c1dc
ray_delpino <raydelpino29@gmail.com>
2017-10-30 14:41:44 -0400

    combine my_list reducer into entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import VideoReducer from './videos_reducer';
 import CategoriesReducer from './categories_reducer';
 import LikesReducer from './likes_reducer';
+import MyListReducer from './my_list_reducer';
 
 const EntitiesReducer = combineReducers({
   videos: VideoReducer,
   categories: CategoriesReducer,
-  likes: LikesReducer
+  likes: LikesReducer,
+  myList: MyListReducer,
 });
 
 export default EntitiesReducer;
