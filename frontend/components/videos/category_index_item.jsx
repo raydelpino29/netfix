d39d16d829a84c553b09c73d4c0ad4c44924d8f0
ray_delpino <raydelpino29@gmail.com>
2017-11-01 08:46:39 -0400

    add margins to catgegory show page

--- frontend/components/videos/category_index_item.jsx
+++ frontend/components/videos/category_index_item.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import VideoItemContainer from './video_item_container';
 
 const CategoryIndexItem = (props) => {
-  debugger
   const videoItems = props.videos.map((video) => {
     return (
       <VideoItemContainer key={video.id} video={video} />
     );
   });
+  let categoryClass = "single-category"
+
+  if (props.classTitle) {
+
+    categoryClass = "single-category show"
+  }
 
   return (
-    <div className="single-category">
+    <div className={categoryClass}>
       <h1 className={props.classTitle}>{props.category.name}</h1>
       <ul className="video-row">
         {videoItems}
