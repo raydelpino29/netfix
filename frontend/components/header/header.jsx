9f2d781c8775385945da1aa4f3e047cef526a578
ray_delpino <raydelpino29@gmail.com>
2017-11-01 08:34:02 -0400

    change browse part of header to be ul

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <nav>
-      <ul className="header">
-        <li>
-          <ul className="header-left">
-            <li className="logo">Netfix</li>
-            <li className='browse'>Browse</li>
-          </ul>
-        </li>
-        <li>
-          <ul className='header-right'>
-            <li className='search'>Search</li>
-            <li className='profile'>Profile</li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  dropdown() {
+    document.getElementsByClassName("browse-items").classList.toggle("reveal");
+  }
+
+  render() {
+    return (
+      <nav>
+        <ul className="header">
+          <li>
+            <ul className="header-left">
+              <li className="logo">Netfix</li>
+              <ul onMouseEnter={this.dropdown} className='browse'>Browse</ul>
+            </ul>
+          </li>
+          <li>
+            <ul className='header-right'>
+              <li className='search'>Search</li>
+              <li className='profile'>Profile</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Header;
