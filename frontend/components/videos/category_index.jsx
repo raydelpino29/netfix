c113178353aa47c6016f729bfb5038328d34613d
ray_delpino <raydelpino29@gmail.com>
2017-11-01 12:01:17 -0400

    refactor category index props

--- frontend/components/videos/category_index.jsx
+++ frontend/components/videos/category_index.jsx
@@ -21,11 +21,7 @@ class CategoryIndex extends React.Component {
         }
       });
       return (
-        <ul key={category.id}><CategoryIndexItem likes={this.props.likes} category={category}
-          videos={videos} createLike={this.props.createLike} deleteLike={this.props.deleteLike}
-          currentUser={this.props.currentUser} createListItem={this.props.createListItem}
-          deleteListItem={this.props.deleteListItem} listItems={this.props.listItems}
-          likedVids={this.props.likedVids} dislikedVids={this.props.dislikedVids} /></ul>
+        <ul key={category.id}><CategoryIndexItem category={category} videos={videos}  /></ul>
       );
     });
 
