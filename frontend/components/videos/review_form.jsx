261e417afe159776dca23777ca18f469604958bb
ray_delpino <raydelpino29@gmail.com>
2017-11-03 08:55:18 -0400

    remove logger

--- frontend/components/videos/review_form.jsx
+++ frontend/components/videos/review_form.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class ReviewForm extends React.Component {
   constructor (props) {
     super(props);
-    debugger
     this.state = { body: "", video_id: this.props.videoId, user_id: this.props.currentUser.id };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
